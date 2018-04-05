09e0efb2b77d83d12ba672dcac7d7667fd595950
Kaushal Rohit <rohitkg98@localhost.localdomain>
2018-04-05 13:36:04 +0530

    started work on css

--- order/views.py
+++ order/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from login.models import Restaurant
-from order.models import Items
+from login.models import Restaurant, Client
+from restaurant.models import Items
+from order.models import Order
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -13,24 +14,23 @@ def select_restaurant(request):
 
 @login_required
 def show_items(request):
-    res_id = request.GET.get('restaurant')
-    restaurant = Restaurant.objects.filter(RID=res_id)[0]
+    res = Restaurant.objects.get(RID=request.POST.get('res_id'))
     items=[]
-    for item in restaurant.Items.all():
+    for item in res.Items.all():
         items.append(item)
     return render(request , 'order/select_items.html', {'items':items} )
 
 @login_required
 def finalize_order(request):
     total_cost = 0
-    selected_item_ids = request.POST.get('selected_item_ids','')
+    selected_item_ids = request.POST.getlist('selected_item_ids')
     user = request.user
-    selected_items=[]
+    client = Client.objects.get(User = user)
+    order = Order( user = client, payment_status ='initiated', time_placed = timezone.now , quantity = len(selected_item_ids) , order_status ='placed' , discount=0 , price = total_cost)
     for id in selected_item_ids:
-        item = (Items.objects.filter(ITEMID=selected_item_ids))[0]
-        selected_items.append(item)
-        total_cost += item.price
-    order = Order(items= selected_items , user = user, payment_status ='initiated', timeplaced = timezone.now , quantity = len(selected_items) , order_status ='placed' , discount=0 , price = total_cost)
+        item = (Items.objects.filter(ITEMID=id))[0]
+        total_cost += item.item_price
+        order.Items.add(item)
     request.session['order']=order.save()
     return render(request , 'order/finalize_order.html', {'item':item })
 
